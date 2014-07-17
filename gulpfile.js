dce481debb3fc199c0f7b1d3ce93a85d09a54deb
andres <code@karateca.com>
2014-07-16 22:06:12 -0400

    new

---
+++ gulpfile.js
@@ -0,0 +1,12 @@
+var gulp = require('gulp')
+var concat = require('gulp-concat')
+
+gulp.task('js', function() {
+  gulp.src(['app/**/*.js'])
+      .pipe(concat('app.js'))
+      .pipe(gulp.dest('dist'))
+});
+
+gulp.task('watch', ['js'], function() {
+  gulp.watch('src/**/*.js', ['js'])
+});
