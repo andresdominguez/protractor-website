21d65fdfa257aff0cba995a0e0ad40d91c6b9774
andres <code@karateca.com>
2014-07-17 21:58:41 -0400

    Add bootstrap

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,12 @@ gulp.task('js', function() {
       .pipe(gulp.dest('app'))
 });
 
+gulp.task('css', function() {
+  gulp.src(['bower_components/bootstrap/dist/css/bootstrap.min.css'])
+      .pipe(concat('styles.css'))
+      .pipe(gulp.dest('app/css'))
+});
+
 gulp.task('watch', ['js'], function() {
   gulp.watch('src/**/*.js', ['js'])
 });
