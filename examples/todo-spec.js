882307b188a334e37e4cc65fb7cde0ef33b8e7ce
andres <code@karateca.com>
2014-07-18 14:30:35 -0400

    Add example

--- examples/todo-spec.js
+++ examples/todo-spec.js
@@ -2,12 +2,10 @@ describe('angularjs homepage todo list', function() {
   it('should add a todo', function() {
     browser.get('http://www.angularjs.org');
 
-    var addTodo = element(by.model('todoText'));
-    var addButton = element(by.css('[value="add"]'));
+    element(by.model('todoText')).sendKeys('write a protractor test');
+    element(by.css('[value="add"]')).click();
 
-    addTodo.sendKeys('write a protractor test');
-    addButton.click();
-
+    var todoList = element.all(by.repeater('todo in todos'));
     expect(todoList.count()).toEqual(3);
     expect(todoList.get(2).getText()).toEqual('write a protractor test');
   });
